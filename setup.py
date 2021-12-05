b6f5d1d9ef53a742579eb38d362813f923eb8fed
kayle <84506518+kaylebetter@users.noreply.github.com>
2021-12-05 04:58:22 +0000

    name changes

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     author="kaylebetter",
     version='1.0',
     description='The most simplistic and easiest wrapper to use for replit\'s database system.',
-    packages=['mydb'],
+    packages=['myreplitdb'],
     include_package_data=True,
     license="MIT",
-    keywords="mydb",
+    keywords="myreplitdb",
     install_requires=[
         'replit',
         'db',
