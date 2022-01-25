014ee54d0022d94f999791972af02374e527dd85
sift <84506518+sifte@users.noreply.github.com>
2022-01-25 17:13:13 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-readme = ""
+README = ""
 with open("README.md") as f:
-    readme = f.read()
+    README = f.read()
 
 setup(
-    name='MyReplitDB',
+    name="MyReplitDB",
     author="kaylebetter",
     url="https://github.com/kaylebetter/myreplitdb",
-    version='1.3',
-    description='An easy to use wrapper for replit\'s database.',
-    long_description=readme,
+    version="1.3",
+    description="An easy to use wrapper for replit's database.",
+    long_description=README,
     long_description_content_type="text/markdown",
-    packages=['myreplitdb'],
+    packages=["myreplitdb"],
     include_package_data=True,
     license="MIT",
-    project_urls={
+    project_urls = {
         "Github": "https://github.com/kaylebetter/myreplitdb",
         },
-    keywords="myreplitdb",
+    keywords="myreplitdb replit db replitdb replit-db",
     install_requires=[
-        'replit',
-        'db',
+        "replit",
     ],
 )
